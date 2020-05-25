0445d4bcd148bca260a50d592cd7296861bac482
Soraya <juliodecolbastos@hotmail.com>
2020-05-25 20:38:31 -0300

    make it better

--- commands/aviso.js
+++ commands/aviso.js
@@ -15,11 +15,10 @@ exports.run = async (client, message, args) => {
   const soraya = client.users.cache.get('594251581789044756');
   const embed = new Discord.MessageEmbed()
   .setColor('#0099ff')
-  .setTitle('<a:christmas_parrot:711787152350117989>Coxinha<a:christmas_parrot:711787152350117989>')
+  .setTitle('<a:christmas_parrot:711787152350117989>Bot aviso<a:christmas_parrot:711787152350117989>')
   .setAuthor('Desocupado-Bot', 'https://i.imgur.com/f5dzzNq.png', 'https://discord.js.org')
   .addFields(
-  { name: 'Começei a vender coxinhas', value: '20 contu a unidade'},
-  { name: 'quem quiser chamo o', value: `${soraya.tag} no privado` }
+  { name: 'Eu estou on novamente', value: 'O Soraya fez melda no meu codigo'},
   )   
   .setTimestamp()
 	.setFooter(`Desenvolvido por: ${soraya.tag} `, soraya.avatarURL());
