948584c0bc80ddd1a2b2b12a6e7eac237a7ac8dd
Soraya <juliodecolbastos@hotmail.com>
2020-05-25 20:28:28 -0300

    make it better

--- commands/aviso.js
+++ commands/aviso.js
@@ -15,11 +15,11 @@ exports.run = async (client, message, args) => {
   const soraya = client.users.cache.get('594251581789044756');
   const embed = new Discord.MessageEmbed()
   .setColor('#0099ff')
-  .setTitle('<a:christmas_parrot:711787152350117989>PobreFlix<a:christmas_parrot:711787152350117989>')
+  .setTitle('<a:christmas_parrot:711787152350117989>Coxinha<a:christmas_parrot:711787152350117989>')
   .setAuthor('Desocupado-Bot', 'https://i.imgur.com/f5dzzNq.png', 'https://discord.js.org')
   .addFields(
-  { name: 'Devido a alguns problemas tecnicos', value: 'não pudemos transmitir o filme'},
-  { name: 'Ate que o Soraya arrume um pc melhor', value: 'não vamos ter PobreFlix'}
+  { name: 'Começei a vender coxinhas', value: '20 contu a unidade'},
+  { name: 'quem quiser chamo o', value: `${soraya.tag} no privado` }
   )   
   .setTimestamp()
 	.setFooter(`Desenvolvido por: ${soraya.tag} `, soraya.avatarURL());
