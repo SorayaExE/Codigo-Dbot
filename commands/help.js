c2b0097f295a0613be1f66f89b57d48bd53ffdb2
Soraya <juliodecolbastos@hotmail.com>
2020-05-26 18:57:36 -0300

    make it better

--- commands/help.js
+++ commands/help.js
@@ -23,7 +23,9 @@ let avatar = message.author.displayAvatarURL({format: "png"});
   { name: 'Para fazer carinho em alguém:', value: 'digite d!carinho (@do usuário)' },
   { name: 'Para pegar um lolicon:', value: 'digite d!lolicon (@do usuário)' }
 )
-  .setFooter(`Desenvolvido por: ${soraya.tag} `, soraya.avatarURL())
+  .setTimestamp()
+  .setFooter(`Desenvolvido por: ${soraya.tag} `, soraya.avatarURL());
+  message.delete().catch(O_o => {});
   message.channel.send('Comandos enviados no seu privado!')
   await message.author.send(embed);
 }
