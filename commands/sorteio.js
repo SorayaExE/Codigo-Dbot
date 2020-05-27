24653a5f74659797a14f980a57f37efafe52ee9e
Soraya <juliodecolbastos@hotmail.com>
2020-05-27 04:22:04 -0300

    make it better

--- commands/sorteio.js
+++ commands/sorteio.js
@@ -11,41 +11,78 @@ module.exports={
             const aembed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`${message.author.username}`)
-            .setDescription('Você é fraco, lhe falta permissão do ademir para usar esse comando')
-            .setFooter('Desenvolvido por Soraya{Savanna}');
+            .setDescription('Você é fraco, lhe falta permissão do ademir para usar esse comando');
             return message.reply(aembed)
         }
         if(!args[0]){
         const bembed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle(`${message.author.username}`)
-        .setDescription('Você não especificou o tempo!')
-        .setFooter('Desenvolvido por Soraya{Savanna}');
+        .setDescription('Você não especificou o tempo!');
         return message.channel.send(bembed)
         }
-        if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) 
-        return message.channel.send(`Você não usou a formatação correta para o horário!`)
-        if(isNaN(args[0][0])) return message.channel.send(`Isso não é um número!`)
+        if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")){
+        const cembed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`${message.author.username}`)
+        .setDescription('Você não usou a formatação correta para o horário!');
+        return message.channel.send(cembed)
+        }
+        if(isNaN(args[0][0])){
+            const dembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${message.author.username}`)
+            .setDescription('Isso não é um número!');
+        return message.channel.send(dembed)
+        }
         let channel = message.mentions.channels.first()
-        if(!channel) return message.channel.send(`Não encontrei esse canal!`)
+        if(!channel){
+            const eembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${message.author.username}`)
+            .setDescription('Não encontrei esse canal!');
+        return message.channel.send(eembed)
+        }
         let prize = args.slice(2).join(" ")
-        if(!prize) return message.channel.send(`Você nao especificou o tempo`)
-        message.channel.send(`*Sorteio criado no ${channel}*`)
+        if(!prize){
+            const fembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${message.author.username}`)
+            .setDescription('Você não especificou o tempo!');
+        return message.channel.send(fembed)
+        }
+        const gembed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`${message.author.username}`)
+        .setDescription(`*Sorteio criado no ${channel}*`);
+        message.channel.send(gembed)
+{
         let Embed = new MessageEmbed()
         .setTitle(`Novo sorteio!`)
         .setDescription(`O ${message.author} está iniciando um sorteio e o premio é **${prize}**`)
         .setTimestamp(Date.now()+ms(args[0]))
-        .setColor(`BLUE`)
+        .setColor('#0099ff')
         let m = await channel.send(Embed)
         m.react("🎉")
+}
         setTimeout(() => {
             if(m.reactions.cache.get("🎉").count<=1){
-                message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`)
-                return message.channel.send(`Poucas pessoas reagiram para ter um vencedor!`)
+                message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`) 
+{
+                    const hembed = new MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(`${message.author.username}`)
+                    .setDescription('Poucas pessoas reagiram para ter um vencedor!');
+                return message.channel.send(hembed)
+                }
             }
             
             let winner = m.reactions.cache.get("🎉").users.cache.filter(u=>!u.bot).random()
-            channel.send(`O vencedor do sorteio que estava valendo **${prize}** é... ${winner}`)
+            const iembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${message.author.username}`)
+            .setDescription(`O vencedor do sorteio que estava valendo **${prize}** é... ${winner}`);
+            channel.send(iembed)
         }, ms(args[0]));
     }
 }
