14d42e8efa8fc53ca29e1bfb307ba48159bb8aaa
Soraya <juliodecolbastos@hotmail.com>
2020-06-01 23:41:21 -0300

    make it better

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, bot, message, args) => {
   function checkDays(date) {
       let now = new Date();
       let diff = now.getTime() - date.getTime();
