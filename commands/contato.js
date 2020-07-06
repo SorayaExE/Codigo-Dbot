1a12e8f4b3969e56bec31c8c69aa0c313472199e
Soraya <juliodecolbastos@hotmail.com>
2020-07-06 04:36:39 -0300

    make it better

---
+++ commands/contato.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message) => {
+    message.channel.send({embed: {
+        color: ('RANDOM'),
+        author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+        },
+        title: "Entre em contato comigo",
+        description: `>Encontrou algum problema? \n>Tem alguma sugestão de comando? \n>Tem alguma duvida? \n>Você pode falar comigo pelo discord <@594251581789044756>`
+    }});
+}
